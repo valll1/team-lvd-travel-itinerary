d33660ccef974bf8e1628a62d9d97b6de4c514ab
valll1 <114962209+valll1@users.noreply.github.com>
2025-05-25 22:20:00 -0400

    Update gradio_ui.py
    added doc string and edited typos

--- src/gradio_ui.py
+++ src/gradio_ui.py
@@ -3,14 +3,15 @@
 import gradio as gr
 from input_processor import process_dates
 
+''' This function creates and returns the user interface using gradio. It recieves the inputted destination city, a trip start date, and the trip end date.'''
 def gradio_interface(): # Defines the gradio_interface function
 
     ui = gr.Interface(
         fn=process_dates, #this is where the ollama response is going to go
         inputs=[
-            gr.Textbox(lines=1, label='Enter your Destination City Here', placeholder = 'e.g Denver, New York'), # Where the user inputs their destination city
-            gr.DateTime(label='Enter your start date Here'), # Where the user inputs their start date
-            gr.DateTime(label='Enter your end date Here') # Where the user inputs their end date
+            gr.Textbox(lines=1, label='Enter your destination city here.', placeholder = 'e.g Denver, New York'), # Where the user inputs their destination city
+            gr.DateTime(label='Enter your start date here.'), # Where the user inputs their start date
+            gr.DateTime(label='Enter your end date here.') # Where the user inputs their end date
         ],
         outputs=gr.Textbox(label='Travel Itinerary'),
         title='Travel Itinerary Generator', # Title of the UI
